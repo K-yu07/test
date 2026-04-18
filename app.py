05ffa3b290e60aa11174a866eb46ad99febf58c7
K-yu07 <anuy.awak.0729@gmail.com>
2026-04-18 19:40:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,25 @@ df = pd.read_excel("quiz.xlsx")
 if "question" not in st.session_state:
     st.session_state.question = df.sample(1).iloc[0]
 
-q = st.session_state.question
+# 選択肢も固定
+if "choices" not in st.session_state:
+    q = st.session_state.question
+    choices = [
+        q["選択肢1"],
+        q["選択肢2"],
+        q["選択肢3"],
+        q["選択肢4"]
+    ]
+    random.shuffle(choices)
+    st.session_state.choices = choices
 
-# 選択肢をシャッフル
-choices = [
-    q["選択肢1"],
-    q["選択肢2"],
-    q["選択肢3"],
-    q["選択肢4"]
-]
-random.shuffle(choices)
+q = st.session_state.question
+choices = st.session_state.choices
 
 # 表示
 st.subheader(q["問題"])
 
-# ラジオボタン（4択）
+# 回答
 user_answer = st.radio("選択してください", choices)
 
 # 判定
@@ -37,4 +41,6 @@ if st.button("答え合わせ"):
 # 次の問題
 if st.button("次の問題"):
     st.session_state.question = df.sample(1).iloc[0]
+    st.session_state.choices = None  # リセット
     st.rerun()
+    
